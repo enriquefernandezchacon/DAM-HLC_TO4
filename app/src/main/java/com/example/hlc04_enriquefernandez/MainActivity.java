f25c789036177a38a40de191ea6fc916b347dbf4
Enrique Fernandez <enriquefernandezchacon@gmail.com>
2023-02-18 15:23:47 +0100

    Falla la descarga de la imagen

--- app/src/main/java/com/example/hlc04_enriquefernandez/MainActivity.java
+++ app/src/main/java/com/example/hlc04_enriquefernandez/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.hlc04_enriquefernandez;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.hlc04_enriquefernandez.servicios.DescargaService;
 import com.example.hlc04_enriquefernandez.servicios.MusicService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +39,35 @@ public class MainActivity extends AppCompatActivity {
             stopService(new Intent(this, MusicService.class));
         });
         btDescarga.setOnClickListener(v -> startService(new Intent(this, DescargaService.class)));
+        
+        crearBroadcastReceiver();
+
+        IntentFilter intentFilter = new IntentFilter(DescargaService.DOWNLOAD_COMPLETE);
+        registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Desregistramos el receptor de broadcast
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
+    }
+
+    private void crearBroadcastReceiver() {
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (action.equals(DescargaService.DOWNLOAD_COMPLETE)) {
+                    byte[] imagen = intent.getByteArrayExtra("imagen");
+                    // Creamos un bitmap a partir del array de bytes
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(imagen, 0, imagen.length);
+                    // Mostramos la imagen en una ImageView
+                    ImageView imageView = findViewById(R.id.imageView);
+                    imageView.setImageBitmap(bitmap);
+                    Toast.makeText(MainActivity.this, "Descarga finalizada", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
     }
 }
--- app/src/main/java/com/example/hlc04_enriquefernandez/servicios/DescargaService.java
+++ app/src/main/java/com/example/hlc04_enriquefernandez/servicios/DescargaService.java
@@ -4,20 +4,54 @@ import android.app.IntentService;
 import android.content.Intent;
 
 import androidx.annotation.Nullable;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class DescargaService extends IntentService {
 
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
-    public DescargaService(String name) {
-        super(name);
+    private static final String URL = "http://http://www.thebiblescholar.com/android_awesome.jpg";
+    public static final String DOWNLOAD_COMPLETE = "com.example.hlc04_enriquefernandez.descargaservice.DOWNLOAD_COMPLETE";
+
+    public DescargaService() {
+        super("DescargaService");
     }
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+        // Descargamos la imagen como array de bytes
+        byte[] imageData = descargarImagen();
+        // Creamos un Intent para notificar la descarga completada
+        Intent completeIntent = new Intent(DOWNLOAD_COMPLETE);
+        // Añadimos los datos de la imagen descargada como extra
+        completeIntent.putExtra("imagen", imageData);
+        // Enviamos el Intent como broadcast local
+        LocalBroadcastManager.getInstance(this).sendBroadcast(completeIntent);
+    }
 
+    private byte[] descargarImagen() {
+        try {
+            // Descargamos la imagen como InputStream
+            java.net.URL url = new URL(URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            // Convertimos el InputStream en un array de bytes
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int n;
+            while ((n = input.read(buffer)) != -1) {
+                output.write(buffer, 0, n);
+            }
+            return output.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
