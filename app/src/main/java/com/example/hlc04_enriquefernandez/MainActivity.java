b4097144fd0d922e43062d8302c6cb598bff680e
Enrique Fernandez <enriquefernandezchacon@gmail.com>
2023-02-18 13:09:47 +0100

    Innitial Commit

---
+++ app/src/main/java/com/example/hlc04_enriquefernandez/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hlc04_enriquefernandez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
